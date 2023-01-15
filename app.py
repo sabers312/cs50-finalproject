d5ad57f6df299a628d1baa698a2d5c9ea6f4e8df
sabers312 <sabry.riache@gmail.com>
2023-01-15 09:57:32 +0100

    first iteration: layout, index, login

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-import from cs50
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        return "<p>Still to do</p>"
+    return render_template("/login.html")
